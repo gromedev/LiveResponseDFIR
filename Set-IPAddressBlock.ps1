9673c205c0fa1104bb52e3de6467d16782e80d9d
gromedev <61883549+gromedev@users.noreply.github.com>
2022-12-13 12:41:34 +0100

    Update Set-IPAddressBlock.ps1

--- Set-IPAddressBlock.ps1
+++ Set-IPAddressBlock.ps1
@@ -1,5 +1,14 @@
+<#
 # IP address you want to block
 hostile_ip = Event.SourceAddress
 
 # Block IP address
 Network.blockIp(hostile_ip)
+#>
+
+#PowerShell equivalent 
+# Get the IP address
+$ipAddress = $Event.SourceAddress
+
+# Block the IP address
+New-NetFirewallRule -DisplayName "Block Malicious IP" -Direction Inbound -Action Block -RemoteAddress $ipAddress

2b15131ae24534dc40c6acccdf6a82e723ac2657
gromedev <61883549+gromedev@users.noreply.github.com>
2022-12-13 12:43:35 +0100

    Update Stop-Process.ps1

--- Stop-Process.ps1
+++ Stop-Process.ps1
@@ -1,5 +1,9 @@
 # Get the process ID
 process_id = Event.ProcessId
 
-# Stop the process
+<# Stop the process
 System.stopProcess(process_id)
+#>
+
+# Stop the process with PowerShell
+Stop-Process -Id $processId -Force

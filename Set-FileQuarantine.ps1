1b0f2e898743891a02dc5ff0e7b12f1ab21579ba
gromedev <61883549+gromedev@users.noreply.github.com>
2022-12-13 13:38:11 +0100

    Update Set-FileQuarantine.ps1

--- Set-FileQuarantine.ps1
+++ Set-FileQuarantine.ps1
@@ -1,2 +1,15 @@
-# script didn't work as intended
-# WIP
+# Set the path to the file that you want to quarantine
+$filePath = "C:\Path\To\File.exe"
+
+# Quarantine the file
+Write-Host "Quarantining $filePath..."
+$response = Invoke-WebRequest -Method Post -Uri "https://localhost:8008/api/v1.0/quarantine" -Body @{
+    "filePaths" = @($filePath)
+} -ContentType "application/json"
+
+# Check the response
+if ($response.StatusCode -eq 200) {
+    Write-Host "Successfully quarantined $filePath"
+} else {
+    Write-Host "Failed to quarantine $filePath"
+}

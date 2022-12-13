3f61a38b31e37dfc62a5a51a5e14149e59ed0b3b
gromedev <61883549+gromedev@users.noreply.github.com>
2022-12-13 12:42:43 +0100

    Update New-Alert.ps1

--- New-Alert.ps1
+++ New-Alert.ps1
@@ -1,5 +1,16 @@
+<#
 # Get details about the event
 event_details = "An event of type '{}' was detected at path '{}'" .format(Event.Type, Event.FilePath)
 
 # Generate an alert
 Alert.generate(event_details)
+#>
+
+
+## PowerShell equivalent
+
+# Get details about the event
+$eventDetails = "An event of type '{0}' was detected at path '{1}'" -f $Event.Type, $Event.FilePath
+
+# Generate an alert
+New-DefenderAlert -Title "Malicious Activity Detected" -Description $eventDetails

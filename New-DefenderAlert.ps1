0c4ee387cf9a382b96538dd61644c4bce573d42f
gromedev <61883549+gromedev@users.noreply.github.com>
2022-12-13 13:03:19 +0100

    Update and rename New-Alert.ps1 to New-DefenderAlert.ps1

--- New-DefenderAlert.ps1
+++ New-DefenderAlert.ps1
@@ -1,14 +1,3 @@
-<#
-# Get details about the event
-event_details = "An event of type '{}' was detected at path '{}'" .format(Event.Type, Event.FilePath)
-
-# Generate an alert
-Alert.generate(event_details)
-#>
-
-
-## PowerShell equivalent
-
 # Get details about the event
 $eventDetails = "An event of type '{0}' was detected at path '{1}'" -f $Event.Type, $Event.FilePath
 
